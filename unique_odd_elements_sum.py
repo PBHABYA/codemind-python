1231a9d177078c847ed1642843163933d7a037d2
P.Bhavya sri <98027854+PBHABYA@users.noreply.github.com>
2023-07-06 11:10:47 +0530

    n=int(input()) s=list(map(int,input().split())) c=[] d=[] for i in s:     if i not in c:         c.append(i) for j in c:     if j%2==1:         d.append(j) print(sum(d))

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,12 @@
+n=int(input())
+s=list(map(int,input().split()))
+c=[]
+d=[]
+for i in s:
+    if i not in c:
+        c.append(i)
+for j in c:
+    if j%2==1:
+        d.append(j)
+print(sum(d))        
+        
