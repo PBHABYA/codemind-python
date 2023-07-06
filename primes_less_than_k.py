abdc2f319db55f072f14174005981ca4195a307f
P.Bhavya sri <98027854+PBHABYA@users.noreply.github.com>
2023-07-06 21:51:19 +0530

    n=int(input()) s=list(map(int,input().split())) k=int(input()) d=[] for i in s:     c=0     for j in range(1,i+1):         if i%j==0:             c+=1     if c==2:         d.append(i) t=0 for i in range(len(d)):     if d[i]<=k:         t+=1 print(t)

---
+++ primes_less_than_k.py
@@ -0,0 +1,17 @@
+n=int(input())
+s=list(map(int,input().split()))
+k=int(input())
+d=[]
+for i in s:
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        d.append(i)
+t=0
+for i in range(len(d)):
+    if d[i]<=k:
+        t+=1
+print(t)        
+        
