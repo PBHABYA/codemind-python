fc30eb12e1ccafc247cbe7f1fe47863fc4787dce
P.Bhavya sri <98027854+PBHABYA@users.noreply.github.com>
2023-07-07 12:00:26 +0530

    n=int(input()) s=list(map(int,input().split())) d=[] for i in s:     c=0     for j in range(1,i+1):         if i%j==0:             c+=1     if c==2:         d.append(i) print(len(d))

---
+++ Primes_in_the_array.py
@@ -0,0 +1,12 @@
+n=int(input())
+s=list(map(int,input().split()))
+d=[]
+for i in s:
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        d.append(i)
+print(len(d))        
+        
